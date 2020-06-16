9a90aee42fd207699fb622e2ac03ec6edb161abe
Bradley Garrod <bradleybredgarrod@gmail.com>
2020-06-16 14:04:22 +0100

    Added tests for new tables

--- api/database_test.go
+++ api/database_test.go
@@ -6,6 +6,23 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 )
 
+func TestCreateTeaTypeTable(t *testing.T) {
+	createTeaTypeString := "CREATE TABLE types"
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Error occurred setting up mock database: %v", err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec(createTeaTypeString).WillReturnResult(sqlmock.NewResult(0, 0))
+
+	createTeaTypeTable(db)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("There were unfulfilled expections: %s", err)
+	}
+}
+
 func TestCreateTeaTable(t *testing.T) {
 	createTeaString := "CREATE TABLE tea"
 	db, mock, err := sqlmock.New()
@@ -14,7 +31,6 @@ func TestCreateTeaTable(t *testing.T) {
 	}
 	defer db.Close()
 
-	mock.ExpectPrepare(createTeaString)
 	mock.ExpectExec(createTeaString).WillReturnResult(sqlmock.NewResult(0, 0))
 
 	createTeaTable(db)
@@ -24,28 +40,36 @@ func TestCreateTeaTable(t *testing.T) {
 	}
 }
 
-// Check that no data will be deleted if the table already exists
-func TestCreateTeaTableExists(t *testing.T) {
-	createTeaString := "CREATE TABLE IF NOT EXISTS tea"
-	rows := sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "test")
+func TestCreateOwnerTable(t *testing.T) {
+	createOwnerString := "CREATE TABLE owner"
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("Error occurred setting up mock database: %v", err)
 	}
-	db.Exec("CREATE TABLE tea (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);")
-	db.Exec("INSERT 'test' INTO tea;")
 	defer db.Close()
 
-	mock.ExpectPrepare(createTeaString)
-	mock.ExpectExec(createTeaString).WillReturnResult(sqlmock.NewResult(0, 0))
-	mock.ExpectQuery("SELECT").WillReturnRows(rows)
-	createTeaTable(db)
-	_, err = db.Query("SELECT * FROM tea;")
-	if err != nil {
-		t.Fatalf("Error reading rows from tea mock: %v", err)
-	}
+	mock.ExpectExec(createOwnerString).WillReturnResult(sqlmock.NewResult(0, 0))
+
+	createOwnerTable(db)
 
 	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("There were unfulfilled expectations: %s", err)
+		t.Errorf("There were unfulfilled expections: %s", err)
+	}
+}
+
+func TestCreateTeaOwnersTable(t *testing.T) {
+	createTeaOwnersString := "CREATE TABLE teaOwners"
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Error occurred setting up mock database: %v", err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec(createTeaOwnersString).WillReturnResult(sqlmock.NewResult(0, 0))
+
+	createTeaOwnersTable(db)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("There were unfulfilled expections: %s", err)
 	}
 }
