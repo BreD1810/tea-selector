fd279834ec1fab277ef61cf61ca7ba20de948dc1
Bradley Garrod <bradleybredgarrod@gmail.com>
2020-06-17 18:33:28 +0100

    Wrote tests for deleteTeaTypeHandler

--- api/handlers_test.go
+++ api/handlers_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 // https://blog.questionable.services/article/testing-http-handlers-go/
@@ -59,7 +61,7 @@ func TestCreateTeaTypeHandler(t *testing.T) {
 		t.Errorf("PUT /type returned wrong status code:\n got: %v\n want: %v", status, http.StatusCreated)
 	}
 
-	expected := "{\"id\":10,\"name\":\"Black Tea\"}"
+	expected := `{"id":10,"name":"Black Tea"}`
 	if actual := rr.Body.String(); actual != expected {
 		t.Errorf("PUT /type returned unexpected body:\n got: %v\n wanted: %v", actual, expected)
 	}
@@ -89,7 +91,7 @@ func TestErrorCreateTeaTypeHandler(t *testing.T) {
 		t.Errorf("PUT /type returned wrong status code:\n got: %v\n want: %v", status, http.StatusBadRequest)
 	}
 
-	expected := "{\"error\":\"Error\"}"
+	expected := `{"error":"Error"}`
 	if actual := rr.Body.String(); actual != expected {
 		t.Errorf("PUT /type returned unexpected body:\n got: %v\n wanted: %v", actual, expected)
 	}
@@ -97,4 +99,69 @@ func TestErrorCreateTeaTypeHandler(t *testing.T) {
 
 func createTeaTypeResponseErrorMock(teaType *TeaType) error {
 	return errors.New("Error")
+}
+
+func TestDeleteTeaTypeHandler(t *testing.T) {
+	req, err := http.NewRequest(http.MethodDelete, "/type", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	vars := map[string]string{"id": "1"}
+	req = mux.SetURLVars(req, vars)
+
+	// Mock the response from the database
+	oldFunc := DeleteTeaTypeFunc
+	defer func() { DeleteTeaTypeFunc = oldFunc }()
+	DeleteTeaTypeFunc = deleteTeaTypeResponseMock
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(deleteTeaTypeHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("DELETE /type returned wrong status code:\n got: %v\n want: %v", status, http.StatusOK)
+	}
+
+	expected := `{"name":"Black Tea","result":"success"}`
+	if actual := rr.Body.String(); actual != expected {
+		t.Errorf("DELETE /type returned unexpected body:\n got: %v\n wanted: %v", actual, expected)
+	}
+}
+
+func deleteTeaTypeResponseMock(teaType *TeaType) error {
+	teaType.Name = "Black Tea"
+	return nil
+}
+
+func TestDeleteTeaTypeErrorHandler(t *testing.T) {
+	req, err := http.NewRequest(http.MethodDelete, "/type", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	vars := map[string]string{"id": "1"}
+	req = mux.SetURLVars(req, vars)
+
+	// Mock the response from the database
+	oldFunc := DeleteTeaTypeFunc
+	defer func() { DeleteTeaTypeFunc = oldFunc }()
+	DeleteTeaTypeFunc = deleteTeaTypeResponseErrorMock
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(deleteTeaTypeHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("DELETE /type returned wrong status code:\n got: %v\n want: %v", status, http.StatusBadRequest)
+	}
+
+	expected := `{"error":"ID does not exist in database"}`
+	if actual := rr.Body.String(); actual != expected {
+		t.Errorf("DELETE /type returned unexpected body:\n got: %v\n wanted: %v", actual, expected)
+	}
+}
+
+func deleteTeaTypeResponseErrorMock(teaType *TeaType) error {
+	return errors.New("sql: Rows are closed")
 }
