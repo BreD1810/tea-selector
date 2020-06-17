97cbf2231e4a265c54756bc226e717dd8fd4174c
Bradley Garrod <bradleybredgarrod@gmail.com>
2020-06-17 16:43:17 +0100

    Added a test for createTeaTypeHandler for when an error is returned when trying to add to database

--- api/handlers_test.go
+++ api/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -67,4 +68,33 @@ func TestCreateTeaTypeHandler(t *testing.T) {
 func createTeaTypeResponseMock(teaType *TeaType) error {
 	teaType.ID = 10
 	return nil
+}
+
+func TestErrorCreateTeaTypeHandler(t *testing.T) {
+	req, err := http.NewRequest(http.MethodPost, "/type", strings.NewReader(`{"name": "Black Tea"}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Mock the response from the database
+	oldFunc := CreateTeaTypeFunc
+	defer func() { CreateTeaTypeFunc = oldFunc }()
+	CreateTeaTypeFunc = createTeaTypeResponseErrorMock
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(createTeaTypeHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("PUT /type returned wrong status code:\n got: %v\n want: %v", status, http.StatusBadRequest)
+	}
+
+	expected := "{\"error\":\"Error\"}"
+	if actual := rr.Body.String(); actual != expected {
+		t.Errorf("PUT /type returned unexpected body:\n got: %v\n wanted: %v", actual, expected)
+	}
+}
+
+func createTeaTypeResponseErrorMock(teaType *TeaType) error {
+	return errors.New("Error")
 }
