d589ed73dfa3686724563417b71bbe428aa93f61
Bradley Garrod <bradleybredgarrod@gmail.com>
2020-06-18 11:11:09 +0100

    Fixed incorrect request type from creating a tea type

--- api/main.go
+++ api/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	router.HandleFunc("/types", getAllTeaTypesHandler).Methods(http.MethodGet)
 	router.HandleFunc("/type/{id:[0-9]+}", getTeaTypeHandler).Methods(http.MethodGet)
-	router.HandleFunc("/type", createTeaTypeHandler).Methods(http.MethodDelete)
+	router.HandleFunc("/type", createTeaTypeHandler).Methods(http.MethodPost)
 	router.HandleFunc("/type/{id:[0-9]+}", deleteTeaTypeHandler).Methods(http.MethodDelete)
 
 	addr := ":" + cfg.Server.Port
