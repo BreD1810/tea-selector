32d151060b8e8b28cfe964e30a4a11d09575eef5
Bradley Garrod <bradleybredgarrod@gmail.com>
2020-06-23 17:57:57 +0100

    Added the ability to delete teas

--- mobileApp/components/TeaManager/TeaListItem.js
+++ mobileApp/components/TeaManager/TeaListItem.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import {
-  Text,
-  TouchableOpacity,
-  View,
-  StyleSheet
-} from 'react-native';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome5';
 
-const TeaListItem = ({item}) => {
+const TeaListItem = ({item, deleteTea}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
         <Text style={styles.listItemText}>{item.name}</Text>
+        <Icon
+          name="trash"
+          size={20}
+          color="firebrick"
+          onPress={() => deleteTea(item.id)}
+        />
       </View>
     </TouchableOpacity>
   );
 };
-
 
 const styles = StyleSheet.create({
   listItem: {
     padding: 15,
     backgroundColor: '#f8f8f8',
     borderWidth: 1,
-    borderColor: '#eee'
+    borderColor: '#eee',
   },
   listItemView: {
-    flexDirection: "row",
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   listItemText: {
-    fontSize: 18
-  }
+    fontSize: 18,
+  },
 });
 
 export default TeaListItem;
--- mobileApp/components/TeaManager/TeaManager.js
+++ mobileApp/components/TeaManager/TeaManager.js
@@ -2,39 +2,69 @@ import React, {useState, useEffect} from 'react';
 import {
   View,
   FlatList,
-  ActivityIndicator
+  ActivityIndicator,
+  Alert,
+  ToastAndroid,
 } from 'react-native';
 import TeaListItem from './TeaListItem';
-import { serverURL } from '../../app.json';
+import {serverURL} from '../../app.json';
 
 const TeaManager = () => {
-
-  const [teas, setTeas] = useState([
-    {id: '1', name: 'Test'},
-    {id: '2', name: 'Test 2'},
-    {id: '3', name: 'Test 3'},
-  ]);
+  const [teas, setTeas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  
+
+  const deleteTea = id => {
+    fetch(serverURL + '/tea/' + id, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.json().error);
+        } else {
+          setTeas(prevTeas => {
+            return prevTeas.filter(tea => tea.id !== id);
+          });
+          ToastAndroid.showWithGravityAndOffset(
+            'Tea successfully deleted!',
+            ToastAndroid.SHORT,
+            ToastAndroid.BOTTOM,
+            0,
+            150,
+          );
+        }
+      })
+      .catch(error => {
+        console.warn(error);
+        Alert.alert(
+          'Error deleting tea',
+          'Please check if someone owns this tea!',
+        );
+      });
+  };
+
   useEffect(() => {
     fetch(serverURL + '/teas')
-    .then((response) => response.json())
-    .then((json) => {
-      setTeas(json);
-    })
-    .catch((error) => console.error(error))
-    .finally(() => {
-      setIsLoading(false);
-    })
+      .then(response => response.json())
+      .then(json => {
+        setTeas(json);
+      })
+      .catch(error => console.error(error))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
-  
+
   return (
     <View>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList 
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
           data={teas}
-          renderItem={({item}) => <TeaListItem item={item} />}
-          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <TeaListItem item={item} deleteTea={deleteTea} />
+          )}
+          keyExtractor={item => item.id.toString()}
         />
       )}
     </View>
