bb2d8f1a0a11c8f24bd34b96ae96d003339a81d5
Bradley Garrod <bradleybredgarrod@gmail.com>
2020-06-26 19:10:26 +0100

    Added a check for if the JWT is still valid.

--- mobileApp/components/JWTManager.js
+++ mobileApp/components/JWTManager.js
@@ -1,14 +1,25 @@
 import {AsyncStorage} from 'react-native';
+import {serverURL} from '../app.json';
 
 const JWTManager = {
   async getJWT(setJWT, setIsAuthorized) {
     try {
       const value = await AsyncStorage.getItem('jwt_token');
       if (value !== null) {
-        console.log(`JWT: ${value}`);
-        console.log(value);
-        setJWT(value);
-        setIsAuthorized(true);
+        console.log('Found JWT');
+        fetch(serverURL + '/owners')
+          .then(response => {
+            if (!response.ok) {
+              console.log('No longer valid');
+              throw new Error(response.statusText);
+            }
+            console.log('Still valid');
+            setJWT(value);
+            setIsAuthorized(true);
+          })
+          .catch(error => {
+            console.warn(error);
+          });
       } else {
         console.log('No JWT');
       }
